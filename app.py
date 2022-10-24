49be701b0a4d770c1b173d020c9ede75fbe2f1c2
Gabriel Coni Felix Reis <gabriel.coni@hotmail.com>
2022-10-24 11:04:43 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -18,6 +18,8 @@ if st.button('Voltar a página principal'):
   div = Div(text=html)
   st.bokeh_chart(div)
 
+  st.markdown("[https://copadomundofifa.herokuapp.com/]")
+
 def add_bg_from_url():
     st.markdown(
          f"""
