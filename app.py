20ec902e3168af7f7e299942086d19568388a024
Gabriel Coni Felix Reis <gabriel.coni@hotmail.com>
2022-11-17 00:36:39 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -14,11 +14,10 @@ import webbrowser
 #with open("style.css") as f:
 #  st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-url = 'https:copadomundofifa.herokuapp.com'
+url = 'https://copadomundofifa.herokuapp.com'
 
 if st.button('Página Inicial'):
-    webbrowser.open_new(url)
-
+    webbrowser.open_new_tab(url)
 
 def add_bg_from_url():
     st.markdown(
@@ -38,8 +37,9 @@ def add_bg_from_url():
 add_bg_from_url()
 
 
-st.title("Copa do Mundo da FIFA 2022")
-st.text("Algoritmo de Machine Learning capaz de prever quem vai ganhar a Copa do Mundo 2022")
+st.markdown("<h1 style='color:#fff;'> Copa do Mundo da FIFA 2022 </h1>", unsafe_allow_html=True)
+
+st.markdown("<p style='color:#fff;'>Algoritmo de Machine Learning capaz de predizer confrontos da Copa do Mundo no Qatar</p>", unsafe_allow_html=True)
 
 df_selecoes = pd.read_csv("Selecoes2022.csv")
 
